768fca59091e3f7fd98be2cc942af35a635d207c
Arthur <jumeroarthur@gmail.com>
2023-11-01 14:11:24 +0300

    first commit

---
+++ jobapp/app/_layout.js
@@ -0,0 +1,32 @@
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
+
+// SplashScreen.preventAutoHideAsync();
+
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "onboarding",
+};
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack initialRouteName="onboarding">
+      <Stack.Screen name="onboarding" />
+      <Stack.Screen name="home" />
+    </Stack>
+  )
+};
+
+export default Layout;
+
